892b0483d4184945649a75e8fee09ab4b6be8bf1
Amuly.aS <amulyasingh617@gmail.com>
2026-04-20 19:26:55 +0530

    added main.cpp code
    Implement main function for treasure hunt maze game with BFS and DFS options.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include "maze.h"
+#include "bfs.h"
+#include "dfs.h"
 
+using namespace std;
+
+int main()
+{
+    cout << "=== TREASURE HUNT MAZE GAME ===" << endl;
+
+    Maze maze;
+    maze.createMaze();
+
+    int choice;
+    cout << "\nChoose Algorithm to find Treasure:" << endl;
+    cout << "1. BFS (Shortest Path)" << endl;
+    cout << "2. DFS (Any Path)" << endl;
+    cout << "Enter choice: ";
+    cin >> choice;
+
+    if (choice == 1)
+    {
+        cout << "\nUsing BFS to find treasure...\n";
+        BFS bfs;
+        bfs.findTreasure(maze);
+    }
+    else if (choice == 2)
+    {
+        cout << "\nUsing DFS to find treasure...\n";
+        DFS dfs;
+        dfs.findTreasure(maze);
+    }
+    else
+    {
+        cout << "Invalid choice!" << endl;
+    }
+
+    return 0;
+}
