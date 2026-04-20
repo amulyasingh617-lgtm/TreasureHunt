a2f9f4513da40572cecdd5c53a36a5812c3e32d5
Amuly.aS <amulyasingh617@gmail.com>
2026-04-20 19:38:01 +0530

    Define Maze class and Point struct in maze.h
    Added Maze class and Point struct for maze representation.

--- maze.h
+++ maze.h
@@ -1,1 +1,24 @@
+#ifndef MAZE_H
+#define MAZE_H
 
+#include <vector>
+using namespace std;
+
+struct Point {
+    int x, y;
+};
+
+class Maze {
+public:
+    int rows, cols;
+    vector<vector<char>> grid;
+    Point start, treasure;
+
+    Maze(int r, int c);
+
+    void generateMaze();
+    void displayMaze();
+    bool isValid(int x, int y);
+};
+
+#endif
